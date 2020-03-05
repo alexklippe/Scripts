c68724398c44b63eb0973028a8afee9de48c1781
Александр Сальников <asalnikov@gp2.loc>
2020-03-05 16:25:39 +0300

    init

---
+++ gp2/scripts/CreateShareLink.sh
@@ -0,0 +1,16 @@
+﻿UHOME="/home"
+USERSUSERS="$(ypcat passwd | cut -d: -f1)"
+for u in $USERSUSERS
+do
+   _dir="${UHOME}/${u}/Desktop"
+   if [ -d "$_dir" ]
+   then
+      #cp -v "$FILE" "$_dir"
+
+      # CHANGE THE OWNER OF THE FILE TO THE USER YOU COPIED TO
+      #chown $(id -un $u):users "$_dir/Госпитализация.desktop"
+	ln -s /home/ОБЩАЯ $_dir/ОБЩАЯ
+	echo $_dir/ОБЩАЯ
+fi
+   done
+
